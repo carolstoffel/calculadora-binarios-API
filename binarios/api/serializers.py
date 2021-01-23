d1c89f43787416d3531bc3e040ca9b865f55b198
Caroline Stoffel <carol_stoffel@hotmail.com>
2021-01-23 18:21:33 -0300

    Refatorado o comentario de BinariosSerializer

--- binarios/api/serializers.py
+++ binarios/api/serializers.py
@@ -11,6 +11,14 @@ class BinariosSerializer(ModelSerializer):
         fields = ('id', 'operacao', 'numero1', 'numero2')
 
     def to_representation(self, data):
+        """
+        No exercício está escrito que a saída deve ser a resposta, 
+        porém achei mais interessante retornar todos os dados inseridos com a resposta.
+        Para retornar apenas o resultado da operação, seria adicionar as linhas abaixo:
+        saida = {}
+        saida['resultado em binario'] = data['resultado em binario']
+        return saida
+        """
         data = super(BinariosSerializer, self).to_representation(data)
 
         if data['operacao'] == '+':
@@ -25,11 +33,3 @@ class BinariosSerializer(ModelSerializer):
             r = mod(int(data['numero1'], 2), int(data['numero2'], 2))
         data['resultado em binario'] = bin(r)[2:]
         return data
-        '''
-        No exercício está escrito que a saída deve ser a resposta, 
-        porém achei mais interessante retornar todos os dados inseridos com a resposta.
-        Para retornar apenas o resultado da operação, seria adicionar as linhas abaixo:
-        saida = {}
-        saida['resultado em binario'] = data['resultado em binario']
-        return saida
-        '''
