5162a1a16b89afbd960c43346e62770c4247e5dd
Caroline Stoffel <carol_stoffel@hotmail.com>
2021-01-25 18:19:33 -0300

    adicionado filtro

--- binarios/api/viewsets.py
+++ binarios/api/viewsets.py
@@ -1,21 +1,19 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from django.conf import settings
 from django.core import serializers
-from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.views import APIView
-#from rest_framework.decorators import api_view
-from django.http import Http404
 from django.shortcuts import render
 from rest_framework import viewsets
 from binarios.models import CalculaBinarios
 from .serializers import BinariosSerializer
-from django.http import JsonResponse
-from rest_framework.decorators import action
+
 
 
 class BinariosViewSet(viewsets.ModelViewSet):
     #queryset = CalculaBinarios.objects.all()
     serializer_class = BinariosSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['id', 'operacao']
 
     def get_queryset(self):
         queryset = CalculaBinarios.objects.all()
--- calculadora/settings.py
+++ calculadora/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'binarios',
 ]
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
